3b3fee26f029c288e8a39d2edd7bf889750e1c96
Alexey Astafev <efsneiron@gmail.com>
2020-04-09 12:28:03 +0300

    init

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="container" :style="style">
+        <div class="text" @click="reverseMessage">
+            {{message}}
+        </div>
+    </div>
+</template>
+
+<script>
+    import "./scss/app.scss"
+    import police from "./assets/police.jpg"
+    import ecilop from "./assets/ecilop.jpg"
+
+    export default {
+        data: function () {
+            return {
+                message: 'police',
+                flg: false,
+                style: {
+                    background: `url(${police}) no-repeat center / cover`
+                }
+            }
+        },
+        methods: {
+            reverseMessage: function () {
+                this.flg = !this.flg
+
+                if (this.flg) {
+                    this.style.background = `url(${ecilop}) no-repeat center / cover`
+                } else {
+                    this.style.background = `url(${police}) no-repeat center / cover`
+                }
+
+                this.message = this.message.split('').reverse().join('')
+            }
+        }
+    }
+</script>
---
+++ webpack.config.js
@@ -0,0 +1,103 @@
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { VueLoaderPlugin } = require('vue-loader');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CSSNano = require('cssnano')
+const Autoprefixer = require('autoprefixer')
+
+const isDev = process.env.NODE_ENV === 'development'
+const isProd = !isDev;
+const getFilename = ext => isProd ? `[name].[hash].${ext}` : `[name].${ext}`
+const getLoaders = (extra) => {
+    const loaders = [
+        MiniCssExtractPlugin.loader,
+        'css-loader',
+    ]
+
+    if (isProd) {
+        loaders.push({
+            loader: "postcss-loader",
+            options: {
+                plugins: [
+                    new Autoprefixer,
+                    new CSSNano
+                ]
+            }
+        })
+    }
+
+    if (extra) {
+        loaders.push(extra)
+    }
+
+    return loaders
+}
+
+module.exports = {
+    context: path.resolve(__dirname, "src"),
+    entry: {
+        app: './index.js'
+    },
+    output: {
+        filename: 'js/' + getFilename('js'),
+        path: path.resolve(__dirname, 'dist'),
+        publicPath: "/"
+    },
+    module: {
+        rules: [
+            {
+                test: /\.vue$/,
+                loader: 'vue-loader',
+                exclude: /node-mudules/
+            },
+            {
+                test: /\.js/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            },
+            {
+                test: '/\.css/',
+                use: getLoaders()
+            },
+            {
+                test: /\.(s[ac]ss)$/,
+                use: getLoaders('sass-loader'),
+                exclude: /node-modules/
+            },
+            {
+                test: /\.(png|jpg)$/,
+                loader: 'file-loader'
+            }
+        ],
+    },
+    devServer: {
+        quiet: true,
+        overlay: true,
+        port: 3000
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new VueLoaderPlugin(),
+        new MiniCssExtractPlugin({
+            filename: 'css/' + getFilename('css'),
+        }),
+        new HtmlWebpackPlugin({
+            template: '../public/index.html',
+            filename: path.resolve(__dirname, 'dist/index.html'),
+            minify: isProd
+        }),
+    ],
+    resolve: {
+        // extensions: ['js', 'css', 'scss', 'json']
+    },
+    optimization: {
+        splitChunks: {
+            chunks: 'all'
+        }
+    }
+};
